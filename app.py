fadaa448bf7f7fa06c365b714753533101b520de
Aminah Ahmed <aminahahmed@Aminahs-MacBook-Air.local>
2020-11-16 11:51:48 +0000

    flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import relevant packages
-from flask import Flask
+from flask import Flask, jsonify
 
 # Create an instance of our app
 app = Flask(__name__)
@@ -12,8 +12,19 @@ students = [
 @app.route("/")  # localhost:5000 this is the default port for Flask
 
 def home():
-    return "This is a dream team of DevOps Spartans!"
+    return "<h1>This is a dream team of DevOps Spartans!</hi>"
 # This function runs when the URL/API is accessed
+
+
+# Creating our own API to display data on the specific route/URL/endpoint.
+@app.route("/api/v1/student/data", methods=["GET"])
+# This will add this API/URL to the http://127.0.0.1:5000/api/v1/student/data
+def customised_api():
+    return jsonify(students)
+    # Apply ETL- Extract Transform Load
+    # jsonify(students) transforms the data in students to JSON
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
